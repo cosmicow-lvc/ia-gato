76e0e5afe78dbbb86689003f0d62c2400c82841b
cosmicow-lvc <maximoalexandro464@gmail.com>
2024-12-09 20:47:10 -0300

    Implementado juego para 2 jugadores

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,7 @@
 #include "Tablerosinpoda.h"
 using namespace std;
 
-void input_tablero(int n, int m, Tablero* tabl){
+void input_tablero_vs_ia(int n, int m, Tablero* tabl){
     if (tabl -> tablero[n][m] == -1){
         if (tabl -> winner == -1){
             tabl -> tablero[n][m] = 0;
@@ -14,13 +14,66 @@ void input_tablero(int n, int m, Tablero* tabl){
     } else {
         cout << "Posicion ocupada" << endl;
     }
+}
+
+void input_tablero_vs_jugador(int n, int m, Tablero* tabl, int jugador){
+    if (tabl -> tablero[n][m] == -1){
+        if (tabl -> winner == -1){
+            tabl -> tablero[n][m] = jugador-1;
+            tabl -> winner = tabl -> ganar_partida();
+        }
+    } else {
+        cout << "Posicion ocupada" << endl;
+    }
     
 }
 
-void partida_normal(){
+void partida_vs_jugador(){
     Tablero* tablero = new Tablero();
-    cout << "Bienvenido al juego de Gato!" << endl;
-    cout << "estos valores tienen los siguientes significados:" << endl;
+    int jugador = 1;
+    cout << "Estos valores tienen los siguientes significados:" << endl;
+    cout << " 0 = primera fila o columna del tablero" << endl;
+    cout << " 1 = segunda fila o columna del tablero" << endl;
+    cout << " 2 = tercera fila o columna del tablero" << endl;
+    cout << "Espacio en blanco entre fichas significa una casilla vacia" << endl;
+    cout << "La ficha del jugador 1 es X, del jugador 2 es O" << endl;
+    while (tablero -> winner == -1){
+        tablero -> mostrar_tablero();
+        string x, y;
+        cout << "Ingrese una fila entre [0,1,2]" << endl;
+        cin >> x;
+        if (x == "2" || x == "1" || x == "0"){
+            cout << "Ingrese una columna entre [0,1,2]" << endl;
+            cin >> y;
+            if (y == "2" || y == "1" || y == "0"){
+                input_tablero_vs_jugador(stoi(x),stoi(y),tablero, jugador);
+                jugador = jugador == 1 ? 2 : 1;
+            } else {
+                cout << "Input invalido" << endl;
+                continue;
+            }
+        } else {
+            cout << "Input invalido" << endl;
+            continue;   
+        }
+        if (tablero -> tablero_lleno()){
+            break;
+        }
+    }
+    tablero -> mostrar_tablero();
+    if (tablero -> winner == 0){
+        cout << "Gano el jugador 1" << endl;
+    } else if (tablero -> winner == 1){
+        cout << "Gano el jugador 2" << endl;
+    } else {
+        cout << "Empate" << endl;
+    }
+    delete tablero;
+}
+
+void partida_vs_ia(){
+    Tablero* tablero = new Tablero();
+    cout << "Estos valores tienen los siguientes significados:" << endl;
     cout << " 0 = primera fila o columna del tablero" << endl;
     cout << " 1 = segunda fila o columna del tablero" << endl;
     cout << " 2 = tercera fila o columna del tablero" << endl;
@@ -35,7 +88,7 @@ void partida_normal(){
             cout << "Ingrese una columna entre [0,1,2]" << endl;
             cin >> y;
             if (y == "2" || y == "0" || y == "1"){
-                input_tablero(stoi(x),stoi(y),tablero);
+                input_tablero_vs_ia(stoi(x),stoi(y),tablero);
             } else {
                 cout << "Input invalido" << endl;
                 continue;
@@ -48,6 +101,7 @@ void partida_normal(){
             break;
         }
     }
+    tablero -> mostrar_tablero();
     if (tablero -> winner == 0){
         cout << "Gano el jugador" << endl;
     } else if (tablero -> winner == 1){
@@ -59,7 +113,23 @@ void partida_normal(){
 }
 
 int main(){
-    partida_normal();
-    
+    string opcion = "-1";
+    while (opcion != "3"){
+        cout << "Menu" << endl;
+        cout << "1. Jugar contra la IA" << endl;
+        cout << "2. Jugar contra otro jugador" << endl;
+        cout << "3. Salir" << endl;
+        cout << "Ingrese una opcion" << endl;
+        cin >> opcion;
+        if (opcion == "1"){
+            partida_vs_ia();
+        } else if (opcion == "2"){
+            partida_vs_jugador();
+        } else if (opcion == "3"){
+            cout << "Saliendo..." << endl;
+        } else {
+            cout << "Opcion invalida" << endl;
+        }
+    }
     return 0;
 }
